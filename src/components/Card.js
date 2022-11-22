9a4ef377ee142cd26921130fba6f706a0fce4af4
spidyansh <aggarwalanshul1997@gmail.com>
2022-11-22 23:46:36 +0530

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <>
+            <div className="row row-cols-1 ">
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <h6 className="card-title">Netflix</h6>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/components/Data.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Data() {
+    const appData=[
+        {
+            imgSource:"",
+            title:"",
+            platform:"",
+            content:"",
+        },
+        {
+            imgSource:"",
+            title:"",
+            platform:"",
+            content:"",
+        },
+        {
+            imgSource:"",
+            title:"",
+            platform:"",
+            content:"",
+        },
+        {
+            imgSource:"",
+            title:"",
+            platform:"",
+            content:"",
+        },
+
+    ]
+  return (
+    <>
+    
+    </>
+  )
+}
